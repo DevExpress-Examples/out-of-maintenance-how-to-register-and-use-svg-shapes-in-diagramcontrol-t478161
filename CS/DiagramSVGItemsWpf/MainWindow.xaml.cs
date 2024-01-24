8e2012f432d4c5b5cd585ab47b72fc2e455aea5b
AndreySlabov <andrey.slabov@devexpress.com>
2024-01-24 15:36:55 +0400

    format

--- CS/DiagramSVGItemsWpf/MainWindow.xaml.cs
+++ CS/DiagramSVGItemsWpf/MainWindow.xaml.cs
@@ -1,64 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using DevExpress.Xpf.Core;
-using System.IO;
+﻿using DevExpress.Xpf.Core;
 using DevExpress.Xpf.Diagram;
-using DevExpress.Diagram.Core;
+using System;
+using System.IO;
+using System.Windows;
 
-namespace DiagramSVGItemsWpf
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : DXWindow
-    {
+namespace DiagramSVGItemsWpf {
+    public partial class MainWindow : ThemedWindow {
         private string path;
-        public MainWindow()
-        {
+        public MainWindow() {
             InitializeComponent();
             path = @"..\..\Icons";
             CreateToolboxItems(path);
         }
 
-        void CreateToolboxItems(string path)
-        {
+        void CreateToolboxItems(string path) {
             var stencil = new DevExpress.Diagram.Core.DiagramStencil("SVGStencil", "IcoMoon - Free Shapes");
             string name = string.Empty;
-            foreach (string file in Directory.GetFiles(path))
-            {
-                try
-                {
-                    name = System.IO.Path.GetFileNameWithoutExtension(file);
-                    using (FileStream stream = File.Open(file, FileMode.Open))
-                    {
+            foreach (string file in Directory.GetFiles(path)) {
+                try {
+                    name = Path.GetFileNameWithoutExtension(file);
+                    using (FileStream stream = File.Open(file, FileMode.Open)) {
                         var shape = DevExpress.Diagram.Core.ShapeDescription.CreateSvgShape(name, name, stream)
-                            .Update(getDefaultSize: () => new System.Windows.Size(100, 100))
-                            .Update(getConnectionPoints: (w, h, p) => new[] { new System.Windows.Point(w / 2, h / 2) });
+                            .Update(getDefaultSize: () => new Size(100, 100))
+                            .Update(getConnectionPoints: (w, h, p) => new[] { new Point(w / 2, h / 2) });
                         stencil.RegisterShape(shape);
                     }
-                }
-                catch (Exception)
-                {
-                    //some SVG files cannot be parsed, so swallow the exception for now.
-                    //throw;
+                } catch (Exception) {
+                    // some SVG files cannot be parsed
                 }
             }
             DevExpress.Diagram.Core.DiagramToolboxRegistrator.RegisterStencil(stencil);
             diagramDesignerControl1.SelectedStencils.Add("SVGStencil");
 
 
-            diagramDesignerControl1.Items.Add(new DiagramShape() { Shape = stencil.GetShape("aid-kit"), Width = 100, Height = 100, Position = new Point(300,300) });
+            diagramDesignerControl1.Items.Add(new DiagramShape() { Shape = stencil.GetShape("aid-kit"), Width = 100, Height = 100, Position = new Point(300, 300) });
         }
     }
 }
